a5147f955ae199c44778bbd4266915d89008b8da
wut5238 <61070254@kmitl.ac.th>
2018-11-24 00:08:11 +0700

    Clean and Analysis data

--- project.py
+++ project.py
@@ -1,46 +1,199 @@
 import pandas as pd
-import csv
-#import matplotlib.pyplot as pt
-from itertools import groupby
 
 """
-ลองใช้ggroupbyแยกปีแยกยี่ห้อ
+Car Sales in Norway Analysis
 
 """
+year = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
 
 df = pd.read_csv('norway_new_car_sales_by_make.csv')
 
-# data_make = df.groupby('Make')#คำสั่งใช้groupbyแยกยี่ห้อ
+#Clean Data
+
 data = df.groupby(['Year', 'Make'])#คำสั่งgroupby แยกปีและรุ่น
+df.set_index(['Year'],inplace=True)
 
-for i in data:
-	print((sum(i['Year']=='2007',i['Make']=='Toyata')).Quantity)
-print(sum(data.get_group((2007, 'Toyota')).Quantity))  # บวกยอดขายรถรุ่นToyota ปี 2007 ได้แล้วโว้ยยยย
-                                                        # ใน get_group ให้กำหนดปีและรุ่นที่ต้องการ
+# กำหนด column ที่เป็นปีที่กำหนดและยี่ห้อรถในปีนั้น
 
-# data_2007 = df[df.Year == 2007]#เอาแค่ของปี2007
+make_2007 = df.loc[:2007, 'Make']
+make_2008 = df.loc[2008:2008, 'Make']
+make_2009 = df.loc[2009:2009, 'Make']
+make_2010 = df.loc[2010:2010, 'Make']
+make_2011 = df.loc[2011:2011, 'Make']
+make_2012 = df.loc[2012:2012, 'Make']
+make_2013 = df.loc[2013:2013, 'Make']
+make_2014 = df.loc[2014:2014, 'Make']
+make_2015 = df.loc[2015:2015, 'Make']
+make_2016 = df.loc[2016:2016, 'Make']
 
+# find top_10 of each year
 
+def year_2007():
+    make = make_2007.unique() # ดึงยี่ห้อรถแต่ละรุ่นในปี 2007
+    total = []
+    lst = dict() # เก็บจำนวนยอดขายของรถแต่ละรุ่น
 
+    for i in range(len(make)):
+        total.append(sum(data.get_group((year[0], make[i])).Quantity))
+        if sum(data.get_group((year[0], make[i])).Quantity) not in lst:
+            lst[sum(data.get_group((year[0], make[i])).Quantity)] = make[i]
+        else:
+            pass
 
+    total.sort(reverse=True)
+    top_10 = total[0:10]
+    top_10_2007 = [(lst[i], i) for i in top_10]
+    return top_10_2007
 
+def year_2008():
+    make = make_2008.unique() # ดึงยี่ห้อรถแต่ละรุ่นในปี 2008
+    total = []
+    lst = dict() # เก็บจำนวนยอดขายของรถแต่ละรุ่น
 
-"""
-ยอดรวมของรถทุกรุ่นในแต่ละปี ตั้งแต่ 2007-2016 ใช้ไฟล์ .txt
+    for i in range(len(make)):
+        total.append(sum(data.get_group((year[1], make[i])).Quantity))
+        if sum(data.get_group((year[1], make[i])).Quantity) not in lst:
+            lst[sum(data.get_group((year[1], make[i])).Quantity)] = make[i]
+        else:
+            pass
 
-"""
+    total.sort(reverse=True)
+    top_10 = total[0:10]
+    top_10_2008 = [(lst[i], i) for i in top_10]
+    return top_10_2008
 
-# file = open('norway_new_car_sales_by_make1.txt')
-# data = csv.reader(file)
-# table = [row for row in data]
-# print(table)
+def year_2009():
+    make = make_2009.unique() # ดึงยี่ห้อรถแต่ละรุ่นในปี 2009
+    total = []
+    lst = dict() # เก็บจำนวนยอดขายของรถแต่ละรุ่น
 
-# gb = groupby(table, lambda x: x[0])
+    for i in range(len(make)):
+        total.append(sum(data.get_group((year[2], make[i])).Quantity))
+        if sum(data.get_group((year[2], make[i])).Quantity) not in lst:
+            lst[sum(data.get_group((year[2], make[i])).Quantity)] = make[i]
+        else:
+            pass
 
-# for key, group in gb:
-#     total = 0
-#     for item in group:
-#         total += int(item[3])
-#     print(item[0], total)
+    total.sort(reverse=True)
+    top_10 = total[0:10]
+    top_10_2009 = [(lst[i], i) for i in top_10]
+    return top_10_2009
 
+def year_2010():
+    make = make_2010.unique() # ดึงยี่ห้อรถแต่ละรุ่นในปี 2010
+    total = []
+    lst = dict() # เก็บจำนวนยอดขายของรถแต่ละรุ่น
 
+    for i in range(len(make)):
+        total.append(sum(data.get_group((year[3], make[i])).Quantity))
+        if sum(data.get_group((year[3], make[i])).Quantity) not in lst:
+            lst[sum(data.get_group((year[3], make[i])).Quantity)] = make[i]
+        else:
+            pass
+
+    total.sort(reverse=True)
+    top_10 = total[0:10]
+    top_10_2010 = [(lst[i], i) for i in top_10]
+    return top_10_2010
+
+def year_2011():
+    make = make_2011.unique() # ดึงยี่ห้อรถแต่ละรุ่นในปี 2011
+    total = []
+    lst = dict() # เก็บจำนวนยอดขายของรถแต่ละรุ่น
+
+    for i in range(len(make)):
+        total.append(sum(data.get_group((year[4], make[i])).Quantity))
+        if sum(data.get_group((year[4], make[i])).Quantity) not in lst:
+            lst[sum(data.get_group((year[4], make[i])).Quantity)] = make[i]
+        else:
+            pass
+
+    total.sort(reverse=True)
+    top_10 = total[0:10]
+    top_10_2011 = [(lst[i], i) for i in top_10]
+    return top_10_2011
+
+def year_2012():
+    make = make_2012.unique() # ดึงยี่ห้อรถแต่ละรุ่นในปี 2012
+    total = []
+    lst = dict() # เก็บจำนวนยอดขายของรถแต่ละรุ่น
+
+    for i in range(len(make)):
+        total.append(sum(data.get_group((year[5], make[i])).Quantity))
+        if sum(data.get_group((year[5], make[i])).Quantity) not in lst:
+            lst[sum(data.get_group((year[5], make[i])).Quantity)] = make[i]
+        else:
+            pass
+
+    total.sort(reverse=True)
+    top_10 = total[0:10]
+    top_10_2012 = [(lst[i], i) for i in top_10]
+    return top_10_2012
+
+def year_2013():
+    make = make_2013.unique() # ดึงยี่ห้อรถแต่ละรุ่นในปี 2013
+    total = []
+    lst = dict() # เก็บจำนวนยอดขายของรถแต่ละรุ่น
+
+    for i in range(len(make)):
+        total.append(sum(data.get_group((year[6], make[i])).Quantity))
+        if sum(data.get_group((year[6], make[i])).Quantity) not in lst:
+            lst[sum(data.get_group((year[6], make[i])).Quantity)] = make[i]
+        else:
+            pass
+
+    total.sort(reverse=True)
+    top_10 = total[0:10]
+    top_10_2013 = [(lst[i], i) for i in top_10]
+    return top_10_2013
+
+def year_2014():
+    make = make_2014.unique() # ดึงยี่ห้อรถแต่ละรุ่นในปี 2014
+    total = []
+    lst = dict() # เก็บจำนวนยอดขายของรถแต่ละรุ่น
+
+    for i in range(len(make)):
+        total.append(sum(data.get_group((year[7], make[i])).Quantity))
+        if sum(data.get_group((year[7], make[i])).Quantity) not in lst:
+            lst[sum(data.get_group((year[7], make[i])).Quantity)] = make[i]
+        else:
+            pass
+
+    total.sort(reverse=True)
+    top_10 = total[0:10]
+    top_10_2014 = [(lst[i], i) for i in top_10]
+    return top_10_2014
+
+def year_2015():
+    make = make_2015.unique() # ดึงยี่ห้อรถแต่ละรุ่นในปี 2015
+    total = []
+    lst = dict() # เก็บจำนวนยอดขายของรถแต่ละรุ่น
+
+    for i in range(len(make)):
+        total.append(sum(data.get_group((year[8], make[i])).Quantity))
+        if sum(data.get_group((year[8], make[i])).Quantity) not in lst:
+            lst[sum(data.get_group((year[8], make[i])).Quantity)] = make[i]
+        else:
+            pass
+
+    total.sort(reverse=True)
+    top_10 = total[0:10]
+    top_10_2015 = [(lst[i], i) for i in top_10]
+    return top_10_2015
+
+def year_2016():
+    make = make_2016.unique() # ดึงยี่ห้อรถแต่ละรุ่นในปี 2016
+    total = []
+    lst = dict() # เก็บจำนวนยอดขายของรถแต่ละรุ่น
+
+    for i in range(len(make)):
+        total.append(sum(data.get_group((year[9], make[i])).Quantity))
+        if sum(data.get_group((year[9], make[i])).Quantity) not in lst:
+            lst[sum(data.get_group((year[9], make[i])).Quantity)] = make[i]
+        else:
+            pass
+
+    total.sort(reverse=True)
+    top_10 = total[0:10]
+    top_10_2016 = [(lst[i], i) for i in top_10]
+    return top_10_2016
